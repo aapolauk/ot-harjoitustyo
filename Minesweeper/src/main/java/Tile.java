6d0a86d1ceaa2a9129f47d9fb9780f42821db355
aapolauk <aapo.laukkarinen@outlook.com>
2019-04-02 16:20:44 +0300

    muokkasin sovellusta

--- Minesweeper/src/main/java/Tile.java
+++ Minesweeper/src/main/java/Tile.java
@@ -6,14 +6,16 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 public class Tile extends StackPane {
+    
+    appLogic logic = new appLogic();
 
     private int x, y;
     private final int size = 40;
     public boolean hasBomb;
-    private boolean isOpen = false;
+    public boolean isOpen = false;
 
-    private final Rectangle border = new Rectangle(size - 2, size - 2);
-    private Text text = new Text();
+    public final Rectangle border = new Rectangle(size - 2, size - 2);
+    public Text text = new Text();
 
     public Tile(int x, int y, boolean hasBomb) {
         this.x = x;
@@ -23,6 +25,7 @@ public class Tile extends StackPane {
         border.setFill(Color.AQUAMARINE);
 
         text.setFont(Font.font(18));
+        
         text.setText(hasBomb ? "X" : "");
         text.setVisible(false);
 
@@ -31,20 +34,10 @@ public class Tile extends StackPane {
         setTranslateX(x * size);
         setTranslateY(y * size);
 
-        setOnMouseClicked(event -> open());
+        setOnMouseClicked(event -> logic.open(this));
     }
     
-    public void open() {
-        if (isOpen) return;
-
-        isOpen = true;
-        text.setVisible(true);
-        border.setFill(null);
-        
-        if (hasBomb) {
-            System.out.println("BOOOM!");
-        }
-    }
+    
 
     public int getX() {
         return x;
---
+++ Minesweeper/src/main/java/appLogic.java
@@ -0,0 +1,121 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
+
+public class appLogic {
+    private final int Width = 800;
+    private final int Height = 600;
+
+    private final int xTiles = 20;
+    private final int yTiles = 15;
+
+    final Tile[][] grid = new Tile[xTiles][yTiles];
+    
+    boolean explosion = false;
+    
+
+    public Pane createField() {
+        Pane root = new Pane();
+        root.setPrefSize(Width, Height);
+
+        for (int y = 0; y < yTiles; y++) {
+            for (int x = 0; x < xTiles; x++) {
+                Tile tile = new Tile(x, y, Math.random() < 0.4);
+
+                grid[x][y] = tile;
+                root.getChildren().add(tile);
+            }
+        }
+
+        for (int y = 0; y < yTiles; y++) {
+            for (int x = 0; x < xTiles; x++) {
+                Tile tile = grid[x][y];
+
+                if (tile.hasBomb) continue;
+
+                long bombs = getNeighbors(tile).stream().filter(t -> t.hasBomb).count();
+
+                if (bombs > 0){
+                    tile.getText().setText(String.valueOf(bombs));
+                    
+                    if(bombs == 1) tile.getText().setFill(Color.BLUE);
+                    if(bombs == 2) tile.getText().setFill(Color.GREEN);
+                    if(bombs == 3) tile.getText().setFill(Color.MEDIUMTURQUOISE);
+                    if(bombs == 4) tile.getText().setFill(Color.BROWN);
+                    if(bombs == 5) tile.getText().setFill(Color.DEEPPINK);
+                    if(bombs == 6) tile.getText().setFill(Color.INDIGO);
+                    if(bombs == 7) tile.getText().setFill(Color.ORANGE);
+                    if(bombs == 8) tile.getText().setFill(Color.CRIMSON);
+                }
+            }
+        }
+        return root;
+    }
+    
+    
+    
+    void open(Tile tile) {
+        
+        if (tile.isOpen && !explosion)return;
+        
+        tile.isOpen = true;
+        tile.text.setVisible(true);
+        tile.border.setFill(null);
+        
+        if (tile.hasBomb && !explosion) {
+            System.out.println("BOOOM!");
+            explosion = true;
+            openAll();
+        }
+    }
+    
+    void openAll(){
+        for (int i = 0; i < grid[0].length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                open(grid[i][j]);
+            }
+        }
+    };
+    
+
+    private List<Tile> getNeighbors(Tile tile) {
+        List<Tile> neighbors = new ArrayList<>();
+
+        int[] coordinates = new int[]{
+            -1, -1,
+            -1, 0,
+            -1, 1,
+            0, -1,
+            0, 1,
+            1, -1,
+            1, 0,
+            1, 1
+        };
+
+        for (int i = 0; i < coordinates.length; i++) {
+            int x_axis = coordinates[i];
+            int y_axis = coordinates[++i];
+
+            int Xnew = tile.getX() + x_axis;
+            int Ynew = tile.getY() + y_axis;
+
+            if (isValidPoint(Xnew, Ynew)) {
+                neighbors.add(grid[Xnew][Ynew]);
+            }
+        }
+
+        return neighbors;
+    }
+    
+    boolean isValidPoint(int x, int y) {
+        return (x >= 0 && x < xTiles && y >= 0 && y < yTiles);
+    }
+
+    public Tile[][] getGrid() {
+        return grid;
+    }
+    
+}
